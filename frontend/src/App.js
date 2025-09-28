dc49bb3ec493496f78f3a676d429832a53a8db70
GonzaloDST <gonzalo.daniel.st@gmail.com>
2025-09-27 19:40:05 -0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,30 @@
 import React, { useEffect, useState } from "react";
 
-const API_URL = "http://lb-prod-55563764.us-east-1.elb.amazonaws.com:8000/products"; // Cambia esto por tu endpoint real
+const API_URL = "http://lb-prod-55563764.us-east-1.elb.amazonaws.com:8000/users"; // Endpoint de users
 
 function App() {
-  const [orders, setOrders] = useState([]);
+  const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch(API_URL)
       .then((res) => res.json())
       .then((data) => {
-        setOrders(data);
+        setUsers(data);
         setLoading(false);
       });
   }, []);
 
   return (
     <div style={{ padding: 32 }}>
-      <h1>Maki Orders</h1>
+      <h1>Maki Users</h1>
       {loading ? (
         <p>Cargando...</p>
       ) : (
         <ul>
-          {orders.map((order) => (
-            <li key={order.id}>
-              <b>ID:</b> {order.id} | <b>Producto:</b> {order.product_name} | <b>Cantidad:</b> {order.quantity}
+          {users.map((user) => (
+            <li key={user.id}>
+              <b>ID:</b> {user.id} | <b>Username:</b> {user.username} | <b>Email:</b> {user.email}
             </li>
           ))}
         </ul>
